255dc7a0455bc69ceac0c58c53319e9d69311d30
Alexandomingos <37353963+Alexandomingos@users.noreply.github.com>
2021-08-22 14:23:56 -0300

    Add suporte the JPA, MySQL and H2

--- src/main/java/com/ale/pontointeligente/PontoInteligenteApplication.java
+++ src/main/java/com/ale/pontointeligente/PontoInteligenteApplication.java
@@ -1,4 +1,4 @@
-package com.kazale.pontointeligente;
+package com.ale.pontointeligente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
